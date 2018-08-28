063da63ebcaed6ba065364c25e5893a229c66e73
Łukasz Otowski <lukaszotowski@interia.pl>
2018-08-28 08:28:54 +0200

    updated powers calculating method

--- powers-grid/script.js
+++ powers-grid/script.js
@@ -40,16 +40,17 @@ window.onload = function () {
     //calculate powers
     let leftHeadLength = document.getElementsByClassName('left-header').length;
 
-    for (rowCounter = 0; rowCounter < leftHeadLength; rowCounter++) {
+    for (rowCounter = 0; rowCounter < 16; rowCounter++) {
         currentIter = rowCounter + 1;
 
-        for (result = 0; result < 7; result++) {
-            let get = document.getElementsByClassName(`data ${currentIter}`);
+        for (result = 0; result < 6; result++) {
+            let headerRow = document.getElementsByClassName('row')[currentIter - 1];
+            let get = headerRow.getElementsByClassName('data');
             let currentPow = result + rowCounter;
             let currentValue = Math.pow(2, currentPow);
-
-//            get[result].textContent = currentValue;
-        };
+                let loop = result - 1;
+            get[result].textContent = currentValue;
+        }
 
     };
 
