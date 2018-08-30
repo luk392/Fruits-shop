24a4313f85a0f2504d6febf04a89b6965d1c250b
Łukasz Otowski <lukaszotowski@interia.pl>
2018-08-30 15:20:14 +0200

    update calculate methods for top headers

--- powers-grid/script.js
+++ powers-grid/script.js
@@ -1,54 +1,61 @@
 window.onload = function () {
-     
+
     const table = document.getElementsByTagName('table')[0];
-    
+
     //create top headers 
-    for(i=0; i<=1; i++){
+    for (i = 0; i <= 1; i++) {
         let tr = document.createElement('tr');
         tr.className += 'row-top';
         table.appendChild(tr);
         let rowHandle = document.getElementsByTagName('tr')[0];
-        
-        for(i=0; i<7; i++){
+
+        for (i = 0; i < 7; i++) {
             let td = document.createElement('td');
             td.className += 'top-header';
             rowHandle.appendChild(td);
         };
     };
-    
+
     //create rows
-    for(i=1; i<=6; i++){
+    for (i = 1; i <= 6; i++) {
         let tr = document.createElement('tr');
         tr.className += `row ${i}`;
         table.appendChild(tr);
         let rowHandle = document.getElementsByTagName('tr')[i];
-        
-        for(i2=0; i2<7; i2++){
+
+        for (i2 = 0; i2 < 7; i2++) {
             let currIter = i2;
             let td = document.createElement('td');
-            if(currIter === 0){
+            if (currIter === 0) {
                 td.className += 'left-header'
-            }else{
+            } else {
                 td.className += `data ${currIter}`;
             }
             rowHandle.appendChild(td);
         }
     };
-    
 
-    
-    //calculate powers
-    let leftHeadLength = document.getElementsByClassName('left-header').length;
 
-    for (rowCounter = 0; rowCounter < 16; rowCounter++) {
+
+    //CALCULATE METHODS
+    //    let leftHeadLength = document.getElementsByClassName('left-header').length;
+
+    for (let result = 0; result < 6; result++) {
+        let headerValue = Math.pow(2,(result));
+        let topHeader = document.getElementsByClassName('top-header');
+        topHeader[result +1].textContent = headerValue;
+    }
+
+    for (let rowCounter = 0; rowCounter < 16; rowCounter++) {
         currentIter = rowCounter + 1;
 
-        for (result = 0; result < 6; result++) {
+        for (let result = 0; result < 6; result++) {
             let headerRow = document.getElementsByClassName('row')[currentIter - 1];
+            let leftHeader = document.getElementsByClassName('left-header')[currentIter - 1];
             let get = headerRow.getElementsByClassName('data');
             let currentPow = result + rowCounter;
             let currentValue = Math.pow(2, currentPow);
-                let loop = result - 1;
+            leftHeader.textContent = Math.pow(2, (currentIter - 1));
             get[result].textContent = currentValue;
         }
 
