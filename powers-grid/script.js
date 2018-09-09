1290ed7fe4783fa74e547f009f5538703b4b8975
Łukasz Otowski <lukaszotowski@interia.pl>
2018-09-09 21:40:47 +0200

    convert all data into object

--- powers-grid/script.js
+++ powers-grid/script.js
@@ -1,79 +1,89 @@
-const table = document.getElementsByTagName('table')[0];
+var powersGrid = {
+    rows: 9,
+    columns: 10,
+    table: document.getElementsByTagName('table')[0],
+    button_add_columns: document.getElementById('add-columns'),
+    button_add_rows: document.getElementById('add-rows'),
 
-//create top headers
-function create_top_headers (){
-    for (i = 0; i <= 1; i++) {
-        let tr = document.createElement('tr');
-        tr.className += 'row-top';
-        table.appendChild(tr);
-        let rowHandle = document.getElementsByTagName('tr')[0];
+    //create top headers
+    create_top_headers() {
+        for (i = 0; i <= 1; i++) {
+            let tr = document.createElement('tr');
+            tr.className += 'row-top';
+            this.table.appendChild(tr);
+            let rowHandle = document.getElementsByTagName('tr')[0];
 
-        for (i = 0; i < 7; i++) {
-            let td = document.createElement('td');
-            td.className += 'top-header';
-            rowHandle.appendChild(td);
-        }
-    }
-}
-//create rows
-function create_rows (){
-    for (i = 1; i <= 6; i++) {
-        let tr = document.createElement('tr');
-        tr.className += `row ${i}`;
-        table.appendChild(tr);
-        let rowHandle = document.getElementsByTagName('tr')[i];
-
-        for (i2 = 0; i2 < 7; i2++) {
-            let currIter = i2;
-            let td = document.createElement('td');
-            if (currIter === 0) {
-                td.className += 'left-header'
-            } else {
-                td.className += `data ${currIter}`;
+            for (i = 0; i < powersGrid.columns; i++) {
+                let td = document.createElement('td');
+                if (i === 0) {
+                    td.className += 'corner-header';
+                    rowHandle.appendChild(td);
+                } else {
+                    td.className += 'top-header';
+                    rowHandle.appendChild(td);
+                }
             }
-            rowHandle.appendChild(td);
         }
-    }
-}
+    },
+    //create rows
+    create_rows() {
+        for (i = 1; i <= this.rows; i++) {
+            let tr = document.createElement('tr');
+            tr.className += `row ${i}`;
+            this.table.appendChild(tr);
+            let rowHandle = document.getElementsByTagName('tr')[i];
 
+            //
+            let columns = powersGrid.columns;
 
-//CALCULATE METHODS
-//    let leftHeadLength = document.getElementsByClassName('left-header').length;
-
-function calculate_top_headers () {
-    for (let result = 0; result < 6; result++) {
-        let headerValue = Math.pow(2,(result));
+            for (i2 = 0; i2 < columns; i2++) {
+                let currIter = i2;
+                let td = document.createElement('td');
+                if (currIter === 0) {
+                    td.className += 'left-header'
+                } else {
+                    td.className += `data ${currIter}`;
+                }
+                rowHandle.appendChild(td);
+            }
+        }
+    },
+    //CALCULATE METHODS
+    calculate_top_headers() {
         let topHeader = document.getElementsByClassName('top-header');
-        topHeader[result +1].textContent = headerValue;
-    }
-}
+        let vall = this.columns - 1;
+        for (let result = 0; result < vall; result++) {
+            let headerValue = Math.pow(2, (result));
+            topHeader[result].textContent = headerValue;
+        };
+    },
+    calculate_data() {
+        for (let rowCounter = 0; rowCounter < this.rows; rowCounter++) {
+            currentIter = rowCounter + 1;
 
-function calculate_data () {
-    for (let rowCounter = 0; rowCounter < 16; rowCounter++) {
-        currentIter = rowCounter + 1;
-
-        for (let result = 0; result < 6; result++) {
-            let headerRow = document.getElementsByClassName('row')[currentIter - 1];
-            let leftHeader = document.getElementsByClassName('left-header')[currentIter - 1];
-            let get = headerRow.getElementsByClassName('data');
-            let currentPow = result + rowCounter;
-            let currentValue = Math.pow(2, currentPow);
-            leftHeader.textContent = Math.pow(2, (currentIter - 1));
-            get[result].textContent = currentValue;
+            for (let result = 0; result < this.columns - 1; result++) {
+                let headerRow = document.getElementsByClassName('row')[currentIter - 1];
+                let leftHeader = document.getElementsByClassName('left-header')[currentIter - 1];
+                let get = headerRow.getElementsByClassName('data');
+                let currentPow = result + rowCounter;
+                let currentValue = Math.pow(2, currentPow);
+                leftHeader.textContent = Math.pow(2, (currentIter - 1));
+                console.log(get[result]);
+                get[result].textContent = currentValue;
+            }
         }
-
+    },
+    add_column() {
+        let currentColumn = document.getElementsByClassName('row 1')[0].getElementsByClassName('data').length;
+        console.log(currentColumn);
     }
 }
 
-function add_column () {
-    let currentColumn = document.getElementsByClassName('row 1')[0].getElementsByClassName('data').length;
-    console.log(currentColumn);
-    
-}
+
 
 window.onload = function () {
-    create_top_headers()
-    create_rows()
-    calculate_top_headers()
-    calculate_data()
+    powersGrid.create_top_headers()
+    powersGrid.create_rows()
+    powersGrid.calculate_top_headers()
+    powersGrid.calculate_data()
 };
